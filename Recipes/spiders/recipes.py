bb11c28b243b4e97b24fb6f15414e51e9196ad6f
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-07-24 18:55:43 +0530

    fixed bugs

--- Recipes/spiders/recipes.py
+++ Recipes/spiders/recipes.py
@@ -12,7 +12,7 @@ class RecipesSpider(scrapy.Spider):
     def start_requests(self):
 
         urls = [
-                #'https://www.allrecipes.com/recipes/233/world-cuisine/asian/indian/?page=1',
+                'https://www.allrecipes.com/recipes/233/world-cuisine/asian/indian/?page=1',
                 'https://www.allrecipes.com/recipes/1874/world-cuisine/asian/indian/appetizers/?internalSource=hub%20nav&referringId=233&referringContentType=Recipe%20Hub&linkName=hub%20nav%20daughter&clickId=hub%20nav%202&page=1',
                 'https://www.allrecipes.com/recipes/1879/world-cuisine/asian/indian/desserts/?internalSource=hub%20nav&referringId=233&referringContentType=Recipe%20Hub&linkName=hub%20nav%20daughter&clickId=hub%20nav%202&page=1',
                 'https://www.allrecipes.com/recipes/1876/world-cuisine/asian/indian/bread/?internalSource=hub%20nav&referringId=233&referringContentType=Recipe%20Hub&linkName=hub%20nav%20daughter&clickId=hub%20nav%202&page=1',
@@ -31,9 +31,6 @@ class RecipesSpider(scrapy.Spider):
                 'https://www.allrecipes.com/recipes/1877/world-cuisine/asian/indian/side-dishes/?internalSource=hub%20nav&referringId=233&referringContentType=Recipe%20Hub&linkName=hub%20nav%20daughter&clickId=hub%20nav%202': 5,
                 'https://www.allrecipes.com/recipes/1879/world-cuisine/asian/indian/desserts/?internalSource=hub%20nav&referringId=233&referringContentType=Recipe%20Hub&linkName=hub%20nav%20daughter&clickId=hub%20nav%202': 3
                }
-
-    # "page_number" CONTAINS THE NEXT PAGE INDEX IN THE URL TO BE CRAWLED
-    page_number = 2
 
     # "reference" IS A DICT THAT HOLDS THE "title" AS KEY AND "url" AS ITS VALUE
     reference = {}
@@ -59,25 +56,49 @@ class RecipesSpider(scrapy.Spider):
             title = recipe.css('span.fixed-recipe-card__title-link::text').extract()
             url = recipe.css('.fixed-recipe-card__h3 a').xpath("@href").extract()
 
-            # OBTAINING THE URL OF NEXT PAGE
-            next_page = response.url[:-1] + str(RecipesSpider.page_number)
-
             # "response.url" HOLDS THE URL OF THE CURRENT CRAWLING PAGE
             # "URL" TAKES A CORY OF THE CURRENT PAGE URL
             URL = str(response.url)
+            
+            # CONVERTING STRING TO LIST
+            URL_temp = list(URL)
+            
+            # REVERSE THE LIST
+            URL_temp.reverse()
+            
+            # CONVERTING REVERSED LIST INTO STRING
+            URL_rev = ''
+            for ele in URL_temp:
+                URL_rev = URL_rev + ele
 
-            # REMOVING "&page=?" SUB-STRING FROM THE MAIN STRING
-            URL = URL[:-7]
+            # GET THE INDEX OF "=" IN THE REVERSED STRING
+            index = URL_rev.index('=')
+            
+            # EXTRACTING THE PAGE NUMBER USING "index" AND CONVERTING IT TO LIST
+            page_number_temp = list(URL_rev[:index])
+            
+            # REVERSE LIST TO GET THE EXACT PAGE NUMBER
+            page_number_temp.reverse()
+            page_number = ''
+            
+            # CONVERTING THE LIST INTO STRING
+            for ele in page_number_temp:
+                page_number = page_number + ele
+
+            # INCREMENT THE "page_number" TO GET PAGE INDEX OF NEXT PAGE
+            page_number = int(page_number) + 1
+            
+            # REMOVING "&page=*" SUB-STRING FROM THE MAIN STRING -> ['&page=' = 6] + index
+            count = 6 + index
+            URL = URL[:-count]
+
+            # OBTAINING THE URL OF NEXT PAGE
+            next_page = response.url[:-index] + str(page_number)
 
             # CRAWL THE NEXT PAGE IF "page_number" AS NOT EXCEDED TOTAL PAGE NUMBER
             # IF "page_number" AS  EXCEDED TOTAL PAGE NUMBER THEN RESET THE "page_number"
-            if RecipesSpider.page_number <= RecipesSpider.url_to_total_pages[URL]:
-                RecipesSpider.page_number += 1
+            if page_number <= RecipesSpider.url_to_total_pages[URL]:
                 yield response.follow(next_page, callback=self.parse)
-
-            else:
-                # RESET THE "page_number"
-                RecipesSpider.page_number = 2
 
             # ADDING TO "reference"
             RecipesSpider.reference[title[0]] = url[0]
