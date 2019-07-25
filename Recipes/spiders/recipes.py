dadc1c3e1eb65f07c0e1205c28429bff81d992b2
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-07-26 00:31:39 +0530

    created a method "generate_next_pageindex" by combining some set of lines

--- Recipes/spiders/recipes.py
+++ Recipes/spiders/recipes.py
@@ -24,7 +24,6 @@ class RecipesSpider(scrapy.Spider):
 
     # "url_to_total_pages" HOLDS THE TOTAL NUMBER OF PAGES OF THE CORRESPONDING URL
     url_to_total_pages = {
-                'https://www.allrecipes.com/recipes/233/world-cuisine/asian/indian/?': 43,
                 'https://www.allrecipes.com/recipes/233/world-cuisine/asian/indian/': 43,
                 'https://www.allrecipes.com/recipes/1874/world-cuisine/asian/indian/appetizers/?internalSource=hub%20nav&referringId=233&referringContentType=Recipe%20Hub&linkName=hub%20nav%20daughter&clickId=hub%20nav%202': 3,
                 'https://www.allrecipes.com/recipes/1876/world-cuisine/asian/indian/bread/?internalSource=hub%20nav&referringId=233&referringContentType=Recipe%20Hub&linkName=hub%20nav%20daughter&clickId=hub%20nav%202': 2,
@@ -45,6 +44,43 @@ class RecipesSpider(scrapy.Spider):
     user = UserInput()
     item_name = user.read()
 
+    # "generate_next_pageindex" METHOD GENERATES 'URL' AND 'NEXT PAGE INDEX'
+    def generate_next_pageindex(self, url):
+
+        # CONVERTING STRING TO LIST
+        url_temp = list(url)
+
+        # REVERSE THE LIST
+        url_temp.reverse()
+
+        # CONVERTING REVERSED LIST INTO STRING
+        url_rev = ''
+        for ele in url_temp:
+            url_rev = url_rev + ele
+
+        # GET THE INDEX OF "=" IN THE REVERSED STRING
+        index = url_rev.index('=')
+
+        # EXTRACTING THE PAGE NUMBER USING "index" AND CONVERTING IT TO LIST
+        page_number_temp = list(url_rev[:index])
+
+        # REVERSE LIST TO GET THE EXACT PAGE NUMBER
+        page_number_temp.reverse()
+        page_number = ''
+
+        # CONVERTING THE LIST INTO STRING
+        for ele in page_number_temp:
+            page_number = page_number + ele
+
+        # INCREMENT THE "page_number" TO GET PAGE INDEX OF NEXT PAGE
+        page_number = int(page_number) + 1
+
+        # REMOVING "&page=*" SUB-STRING FROM THE MAIN STRING -> ['&page=' = 6] + index
+        count = 6 + index
+        url = url[:-count]
+
+        return url, page_number, index
+
     def parse(self, response):
 
         # SELECT THE LIST OF BLOCKS
@@ -53,55 +89,25 @@ class RecipesSpider(scrapy.Spider):
         # LOOP THROUGH EACH BLOCK AND SCRAP THE "title" AND "url" OF THAT BLOCK
         for recipe in recipes_list:
 
-            title = recipe.css('span.fixed-recipe-card__title-link::text').extract()
-            url = recipe.css('.fixed-recipe-card__h3 a').xpath("@href").extract()
+            titles = recipe.css('span.fixed-recipe-card__title-link::text').extract()
+            urls = recipe.css('.fixed-recipe-card__h3 a').xpath("@href").extract()
+
+            # ADDING TO "reference"
+            RecipesSpider.reference[titles[0]] = urls[0]
 
             # "response.url" HOLDS THE URL OF THE CURRENT CRAWLING PAGE
             # "URL" TAKES A CORY OF THE CURRENT PAGE URL
-            URL = str(response.url)
-            
-            # CONVERTING STRING TO LIST
-            URL_temp = list(URL)
-            
-            # REVERSE THE LIST
-            URL_temp.reverse()
-            
-            # CONVERTING REVERSED LIST INTO STRING
-            URL_rev = ''
-            for ele in URL_temp:
-                URL_rev = URL_rev + ele
+            url = str(response.url)
 
-            # GET THE INDEX OF "=" IN THE REVERSED STRING
-            index = URL_rev.index('=')
-            
-            # EXTRACTING THE PAGE NUMBER USING "index" AND CONVERTING IT TO LIST
-            page_number_temp = list(URL_rev[:index])
-            
-            # REVERSE LIST TO GET THE EXACT PAGE NUMBER
-            page_number_temp.reverse()
-            page_number = ''
-            
-            # CONVERTING THE LIST INTO STRING
-            for ele in page_number_temp:
-                page_number = page_number + ele
-
-            # INCREMENT THE "page_number" TO GET PAGE INDEX OF NEXT PAGE
-            page_number = int(page_number) + 1
-            
-            # REMOVING "&page=*" SUB-STRING FROM THE MAIN STRING -> ['&page=' = 6] + index
-            count = 6 + index
-            URL = URL[:-count]
+            url, page_number, index = self.generate_next_pageindex(url)
 
             # OBTAINING THE URL OF NEXT PAGE
             next_page = response.url[:-index] + str(page_number)
 
             # CRAWL THE NEXT PAGE IF "page_number" AS NOT EXCEDED TOTAL PAGE NUMBER
             # IF "page_number" AS  EXCEDED TOTAL PAGE NUMBER THEN RESET THE "page_number"
-            if page_number <= RecipesSpider.url_to_total_pages[URL]:
+            if page_number <= RecipesSpider.url_to_total_pages[url]:
                 yield response.follow(next_page, callback=self.parse)
-
-            # ADDING TO "reference"
-            RecipesSpider.reference[title[0]] = url[0]
 
         # FILTERING THE URL's WHICH ARE NOT REQURIED
         result = RecipesSpider.filter.optimize_search_result(RecipesSpider.item_name, RecipesSpider.reference)
