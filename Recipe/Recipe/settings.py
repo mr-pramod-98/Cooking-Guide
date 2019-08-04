0cff149d1d808f231ad1fd0fcca4fa58eaff54b6
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-08-04 20:13:53 +0530

    Update settings.py

--- Recipe/Recipe/settings.py
+++ Recipe/Recipe/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wz!io*gyyb*ou=t+e2i)rhce497)*yq6x5g%l576o8x=7l$f56'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
