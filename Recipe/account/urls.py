f91fb2dcb8591d3bea34456300f138e565833d7c
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-08-05 21:34:46 +0530

    removed login page from the main page previlage

--- Recipe/account/urls.py
+++ Recipe/account/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('register', views.register, name='register'),
-    path('', views.login, name='login'),
+    path('login', views.login, name='login'),
     path('logout', views.logout, name='logout')
 ]
