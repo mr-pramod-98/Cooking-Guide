5e4ba972632b38dea1d8287e56897ff17a758674
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-08-08 21:49:15 +0530

    adding "account" app to INSTALLED_APPS

--- Recipe/Recipe/settings.py
+++ Recipe/Recipe/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',
     'cook.apps.CookConfig',
     'django.contrib.admin',
     'django.contrib.auth',
