3aa29b5b694377045e43fb5f7583759f61b1e375
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-08-08 21:56:24 +0530

    created cook method

--- Recipe/cook/views.py
+++ Recipe/cook/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import RecipesDjango
 
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+
+def cook(request):
+
+    # FETCHING DATA FROM DATABASE
+    cooking_recipes = RecipesDjango.objects.all()
+    return render(request, "cook.html", {'cooking_recipes': cooking_recipes})
