96cc8bc56570ff0a53a1beda5aba1192450595fd
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-08-10 22:13:58 +0530

    cook_guide fetch's data using CookGuide class method's.

--- Recipe/cook/views.py
+++ Recipe/cook/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
-from .models import RecipesDjango
+import mysql.connector
+
+
+class CookingGuide:
+
+    def __init__(self):
+        self.create_connection()
+
+    def create_connection(self):
+        self.mydatabase = mysql.connector.connect(host='localhost', user='root', passwd='password', database='recipe_test')
+        self.mycursor = self.mydatabase.cursor()
+
+    def fetch_data(self, table_name):
+        # "cooking_recipes" CONTAINS LIST OF DICTIONARIES
+        cooking_recipes = []
+
+        # "table_name" IS NAME OF THE TABLE FROM WHICH DATA HAS TO BE FETCHED
+        self.mycursor.execute(''' SELECT * FROM ''' + table_name)
+
+        # "id", "title", "ingredients" AND "directions" ARE THE COLUMN-NAME'S IN THE TABLE
+        for (id, title, ingredients, directions) in self.mycursor:
+            recipes = {
+                        "title": title,
+                        "ingredients": ingredients,
+                        "directions": directions
+                       }
+            cooking_recipes.append(recipes)
+
+        # RETURNING LIST OF RECIPES AS LIST OF DICTIONARY
+        return cooking_recipes
 
 
 # Create your views here.
@@ -7,8 +36,14 @@ def index(request):
     return render(request, "index.html")
 
 
-def cook(request):
+def cook_guide(request, username):
 
-    # FETCHING DATA FROM DATABASE
-    cooking_recipes = RecipesDjango.objects.all()
+    # "username" IS THE NAME OF THE TABLE
+    data = CookingGuide()
+    cooking_recipes = data.fetch_data(username)
+    return render(request, "cook_guide.html", {'cooking_recipes': cooking_recipes})
+
+
+def cook(request):
+    cooking_recipes = []
     return render(request, "cook.html", {'cooking_recipes': cooking_recipes})
