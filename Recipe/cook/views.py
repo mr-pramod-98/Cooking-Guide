6215d1ef785b45db04b172bbf5545508bbcffc9f
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-08-12 11:26:15 +0530

    added comments.

--- Recipe/cook/views.py
+++ Recipe/cook/views.py
@@ -11,7 +11,9 @@ class CookingGuide:
         self.mydatabase = mysql.connector.connect(host='localhost', user='root', passwd='password', database='recipe_test')
         self.mycursor = self.mydatabase.cursor()
 
+    # "fetch_data" METHOD IS USED TO FETCH DATA FROM THE SPECIFIED TABLE IN THE SPECIFIED DATABASE
     def fetch_data(self, table_name):
+        
         # "cooking_recipes" CONTAINS LIST OF DICTIONARIES
         cooking_recipes = []
 
@@ -32,10 +34,13 @@ class CookingGuide:
 
 
 # Create your views here.
+
+# "index" METHOD IS CALLED WHET THE USER REQUEST FOR "index" PAGE 
 def index(request):
     return render(request, "index.html")
 
 
+# "cook_guide" METHOD IS CALLED WHET THE USER LOGIN'S SUCCESSFULLY
 def cook_guide(request, username):
 
     # "username" IS THE NAME OF THE TABLE
