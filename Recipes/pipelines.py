f8f4ccfeec6af19e4192912ed5e8c48336fe2aa3
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-08-15 00:41:07 +0530

    Removed create_table() method and changed store_to_database method.

--- Recipes/pipelines.py
+++ Recipes/pipelines.py
@@ -5,28 +5,23 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 import mysql.connector
+from .spiders.user_input import UserInput
 
 
 class RecipesPipeline(object):
 
     def __init__(self):
         self.create_connection()
-        self.create_table()
 
     def create_connection(self):
         self.mydatabase = mysql.connector.connect(host='localhost', user='root', passwd='password', database='recipe_test')
         self.mycursor = self.mydatabase.cursor()
 
-    # "create_table" METHOD CREATE's A TABLE AND IF THE TABLE IS ALREADY PRESENT THEN IT DROP THE TABLE AND THEN CREATE
-    def create_table(self):
-        self.mycursor.execute("""DROP TABLE IF EXISTS Recipes""")
-        self.mycursor.execute("""create table Recipes(
-                              title varchar(100),
-                              ingredients text(500),
-                              procedures text
-                             )""")
-
     def store_to_database(self, item):
+
+        # FETCHING TABLE NAME BY USING "get_tablename" METHOD
+        table = UserInput()
+        table_name = UserInput.get_tablename(table)
 
         # CONVERTING THE LIST OF INGREDIENTS INTO STRING
         ingredients = ''
@@ -38,7 +33,7 @@ class RecipesPipeline(object):
         for procedure in item['procedures']:
             procedures = procedures + procedure
 
-        query = "insert into Recipes(title, ingredients, procedures) values(%s, %s, %s)"
+        query = "insert into " + table_name + "(title, ingredients, directions) values(%s, %s, %s)"
         values = (item['title'], ingredients, procedures)
 
         self.mycursor.execute(query, values)
