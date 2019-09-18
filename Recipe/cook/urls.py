63c62f36b077f0d8a1fb78cea80bff02b085b982
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-09-18 18:06:16 +0530

    added one more path which maps the delete_account() method on request to delete the existing user account

--- Recipe/cook/urls.py
+++ Recipe/cook/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('cook_guide/<str:username>/', views.cook_guide, name="cook_guide"),
     path('cook_guide/<str:username>/delete/<str:title>/', views.delete, name="delete_item"),
+    path('cook_guide/<str:username>/delete_account/', views.delete_account, name="delete_account"),
 ]
