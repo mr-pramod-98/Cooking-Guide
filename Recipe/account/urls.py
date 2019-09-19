c2ed2c0dae0707fbb997a6c595abe743a47ee700
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-09-19 23:07:04 +0530

    added one more path which maps UserValidation page to user_validation method

--- Recipe/account/urls.py
+++ Recipe/account/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('register', views.register, name='register'),
     path('login', views.login, name='login'),
-    path('logout', views.logout, name='logout')
+    path('logout', views.logout, name='logout'),
+    path('userValidation/<str:username>/<str:email>', views.user_validation, name='userValidation'),
 ]
